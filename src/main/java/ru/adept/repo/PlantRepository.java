2a3f07682e19321d3968e524b4b40d145d73e913
Nicolay Golovnev <kolya.golovnev@mail.ru>
2022-03-01 00:29:38 +0700

    Созданы репозитории для сущностей

---
+++ src/main/java/ru/adept/repo/PlantRepository.java
@@ -0,0 +1,7 @@
+package ru.adept.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.adept.entity.Plant;
+
+public interface PlantRepository extends JpaRepository<Plant, Long> {
+}
