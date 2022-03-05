d5d80e47e35ee0a48b17e055332106d3e18f9a01
Nikolay Golovnev <kolya.golovnev@mail.ru>
2022-03-05 21:59:38 +0700

    Реализовано заполнение данных на страницу редактирования и запрос на удаление

--- src/main/java/ru/adept/controller/PlantController.java
+++ src/main/java/ru/adept/controller/PlantController.java
@@ -3,12 +3,17 @@ package ru.adept.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.adept.entity.Plant;
+import ru.adept.entity.Preserve;
 import ru.adept.repo.PlantRepository;
+import ru.adept.repo.PreserveRepository;
 import ru.adept.repo.PreservesNPlantsRepository;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PlantController {
@@ -17,6 +22,9 @@ public class PlantController {
 
     @Autowired
     PreservesNPlantsRepository pNpRepository;
+
+    @Autowired
+    PreserveRepository preserveRepository;
 
     @GetMapping(value = {"/", "/index-admin"})
     public String loadIndex(Model model, HttpServletRequest request) {
@@ -27,6 +35,25 @@ public class PlantController {
             return "index";
         else
             return "index-admin";
+    }
+
+    @GetMapping("/edit-herb/{id}")
+    public String editHerb(@PathVariable Long id, Model model) {
+        Optional<Plant> plant = plantRepository.findById(id);
+        if (plant.isPresent())
+            model.addAttribute("plant", plant.get());
+        else
+            model.addAttribute("plant", new Plant());
+        model.addAttribute("herbId", id);
+        List<Preserve> preserves = preserveRepository.findAll();
+        model.addAttribute("preserves", preserves);
+        return "edit-herb";
+    }
+
+    @DeleteMapping("/herb/{id}")
+    public String deleteHerb(@PathVariable Long id) {
+        plantRepository.deleteById(id);
+        return "redirect:/index-admin";
     }
 
 
